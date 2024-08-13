5fccb58de7310e65cb50398bea3f35c805c357ca
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2024-08-13 16:32:47 +0900

    chore: use intellij platform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,51 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 val group: String by project
 val version: String by project
+val platformType: String by project
+val platformVersion: String by project
 
 plugins {
     java
     kotlin("jvm") version "2.0.10"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
 }
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+
+    intellijPlatform {
+        create(platformType, platformVersion)
+
+        plugin("IdeaVIM:2.15.3")
+
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Platform)
+    }
 
     testImplementation("junit", "junit", "4.13.2")
 }
 
-tasks.withType<KotlinJvmCompile> {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-    }
-}
-
-tasks.withType<JavaCompile> {
-    options.release.set(17)
-}
-
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(22))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
-intellij {
-    version.set("2024.1.4")
-    plugins.set(listOf("IdeaVIM:2.15.2"))
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
